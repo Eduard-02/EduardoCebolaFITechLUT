982bace65f7fe6b1dbd38abcb7827032fcca22d8
Eduard-02 <eduardocebola83@gmail.com>
2025-05-03 22:25:50 +0300

    This is what I have so far from the NodeJS module with the video material. (pushed so I won't lost it.)

---
+++ course_work/node/index.js
@@ -0,0 +1,9 @@
+import getPosts, {getPostsLenght} from './postController.js';
+
+console.log(getPosts());
+console.log(getPostsLenght());
+
+/* const { generateRandomNumber, celciusToFahrenheit } = require('./utils');
+
+console.log(`Random Number: ${generateRandomNumber()}`);
+console.log(`Celcius to fahrenheit: ${celciusToFahrenheit(0)}`); */
