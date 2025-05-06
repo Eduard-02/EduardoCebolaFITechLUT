3edc17c921a0db9397e03f6d3eac961095a190b1
Eduard-02 <eduardocebola83@gmail.com>
2025-05-06 16:16:24 +0300

    MongoDB Module

---
+++ course_work/mongo/post.js
@@ -0,0 +1,43 @@
+post =
+[
+{
+    title: 'Post 1',
+    body: 'Body of post',
+    category: 'News',
+    likes: 1,
+    tags: ['news', 'events'],
+    date: Date()
+},
+{
+    title: 'Post 2',
+    body: 'Body of post',
+    category: 'Event',
+    likes: 2,
+    tags: ['news', 'events'],
+    date: Date()
+},
+{
+    title: 'Post 3',
+    body: 'Body of post',
+    category: 'Tech',
+    likes: 3,
+    tags: ['news', 'events'],
+    date: Date()
+},
+{
+    title: 'Post 4',
+    body: 'Body of post',
+    category: 'Event',
+    likes: 4,
+    tags: ['news', 'events'],
+    date: Date()
+},
+{
+    title: 'Post 5',
+    body: 'Body of post',
+    category: 'News',
+    likes: 5,
+    tags: ['news', 'events'],
+    date: Date()
+}
+]
