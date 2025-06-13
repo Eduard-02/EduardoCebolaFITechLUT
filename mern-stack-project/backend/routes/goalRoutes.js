6a227ffb87fe100e0489bca747493256fcd9b029
Eduard-02 <eduardocebola83@gmail.com>
2025-06-13 12:05:59 +0100

    Backend project done

--- mern-stack-project/backend/routes/goalRoutes.js
+++ mern-stack-project/backend/routes/goalRoutes.js
@@ -1,8 +1,9 @@
 const express = require('express')
 const router = express.Router()
 const { getGoals, postGoal, putGoal, deleteGoal } = require('../controllers/goalController')
+const { protect } = require('../middleware/authMiddleware')
 
-router.route('/').get(getGoals).post(postGoal)
-router.route('/:id').put(putGoal).delete(deleteGoal)
+router.route('/').get(protect, getGoals).post(protect, postGoal)
+router.route('/:id').put(protect, putGoal).delete(protect, deleteGoal)
 
 module.exports = router
